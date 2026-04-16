f4f6b9807e0219225998fac872954ed04741a5d4
student1040-cmd <student1040@otfk.ukr.education>
2026-04-16 12:33:46 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,11 @@
-const { defineConfig } = require('vite');
-const vue = require('@vue/plugin-vue');
+import { defineConfig } from 'vite'
+import vue from '@vue/plugin-vue'
 
-module.exports = defineConfig({
+export default defineConfig({
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
     port: 5173,
     allowedHosts: ['laba-2-dgdr.onrender.com']
   }
-});
+})
